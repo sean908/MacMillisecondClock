18585fc1f92e133d4d182d6514f92f1c36fe63ea
Se@n <admin@2088.eu.org>
2026-07-06 13:25:26 +0800

    build: add mac app packaging script

---
+++ scripts/package-app.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -eu
+
+APP_NAME="MacMillisecondClock"
+BUNDLE_ID="eu.2088.MacMillisecondClock"
+CONFIGURATION="release"
+APP_DIR="dist/${APP_NAME}.app"
+CONTENTS_DIR="${APP_DIR}/Contents"
+MACOS_DIR="${CONTENTS_DIR}/MacOS"
+RESOURCES_DIR="${CONTENTS_DIR}/Resources"
+
+swift build -c "${CONFIGURATION}" --product "${APP_NAME}"
+
+rm -rf "${APP_DIR}"
+mkdir -p "${MACOS_DIR}" "${RESOURCES_DIR}"
+
+cp ".build/${CONFIGURATION}/${APP_NAME}" "${MACOS_DIR}/${APP_NAME}"
+chmod +x "${MACOS_DIR}/${APP_NAME}"
+
+cat > "${CONTENTS_DIR}/Info.plist" <<PLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleDevelopmentRegion</key>
+    <string>en</string>
+    <key>CFBundleExecutable</key>
+    <string>${APP_NAME}</string>
+    <key>CFBundleIdentifier</key>
+    <string>${BUNDLE_ID}</string>
+    <key>CFBundleInfoDictionaryVersion</key>
+    <string>6.0</string>
+    <key>CFBundleName</key>
+    <string>${APP_NAME}</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleShortVersionString</key>
+    <string>1.0.0</string>
+    <key>CFBundleVersion</key>
+    <string>1</string>
+    <key>LSMinimumSystemVersion</key>
+    <string>13.0</string>
+    <key>LSUIElement</key>
+    <true/>
+    <key>NSHighResolutionCapable</key>
+    <true/>
+</dict>
+</plist>
+PLIST
+
+codesign --force --sign - "${APP_DIR}" >/dev/null
+
+echo "${APP_DIR}"
